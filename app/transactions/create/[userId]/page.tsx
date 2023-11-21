ce7a9ce557258bf212dc320382048f5f92e2b9de
Victorio Q. Biloy Jr <vbiloy@gmail.com>
2023-11-21 11:13:15 +0800

    Add details for transaction

--- app/transactions/create/[userId]/page.tsx
+++ app/transactions/create/[userId]/page.tsx
@@ -1,18 +1,34 @@
-'use client'
-
 import { createLog } from '@/app/transactions/actions'
+import { getUser } from '@/app/models/user'
 import SubmitButton from '@/app/components/SubmitButton'
 import Link from 'next/link'
 import { FaArrowLeft } from "react-icons/fa"
 
-const CreatePage = ({ params }: { params: { userId: string } }) => {
+const CreatePage = async ({ params }: { params: { userId: string } }) => {
+	const user = await getUser(params.userId);
 	const types = ["CHECK_IN", "CHECK_OUT", "BREAK"]
-
+	const { id, name } = {...user};
 
 	return (
 		<>
 			<form
 				action={createLog}>
+				<div
+					className="mb-6">
+					<label
+						htmlFor="name"
+						className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+						Name
+					</label>
+					<input
+						type="text"
+						id="name"
+						name="name"
+						className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+						defaultValue={name??''}
+						readOnly
+						required />
+				</div>
 				<div
 					className="mb-6">
 					<label
@@ -51,7 +67,7 @@ const CreatePage = ({ params }: { params: { userId: string } }) => {
 					type="hidden"
 					id="userId"
 					name="userId"
-					value={params.userId}
+					defaultValue={id}
 				/>
 				<button
 					className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800 mr-2"
@@ -61,11 +77,6 @@ const CreatePage = ({ params }: { params: { userId: string } }) => {
 						<FaArrowLeft size={20} /> 
 					</Link>
 				</button>
-				{/* <button
-					type="submit"
-					className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-				>Submit
-				</button> */}
 				<SubmitButton />
 			</form>
 		</>
